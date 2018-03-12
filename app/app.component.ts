12cf7421fd64042a252cf1029761414aeb1ee4f1
Niklas Niemelä <niklas.niemela@hotmail.com>
2018-03-12 19:41:18 +0200

    Revert "First commit"
    This reverts commit f61fe10917c03568e9b493055223762875f161a7.

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-import {TrainsComponent} from './trains.component';
-
-@Component({
-    selector: 'my-app',
-    template: '<h1>CRAZY TRAIN</h1><trains></trains>',
-    directives: [TrainsComponent]
-})
-export class AppComponent { }
--- app/train.service.ts
+++
@@ -1,7 +0,0 @@
-
-
-export class TrainService {
-    getTrains(): string[]{
-        return  ["Train1", "Train2", "Train3"];
-    }
-}
